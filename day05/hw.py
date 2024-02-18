d7098ef60068ca628858dcb317cfc4f601638557
Gelbakh30 <vakogelbakh1@gmail.com>
2024-02-18 22:57:17 +0400

    day 05

---
+++ day05/hw.py
@@ -0,0 +1,7 @@
+age = input("Enter your age: ")
+name = input("enter your username: ")
+age = int(age) #converting the input string to an integer
+name = str(name)
+print("your age is:", age)
+print("your name is:", name)
+print("Your personal information has been completed")
